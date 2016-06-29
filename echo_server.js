bfc796d2f5df01732c5d557ffbd434414789ae62
Robby Kim <robert.st.kim@gmail.com>
2016-06-29 13:51:19 -0400

    added input from joe for afternoon sesh

--- echo_server.js
+++ echo_server.js
@@ -4,9 +4,8 @@ var express = require('express');
 var app = express();
 
 app.get('/headers', function (request, response) {
-  var headers = request.headers;
   response.json({
-    headers
+    header: request.headers
   });
 });
 
@@ -14,7 +13,7 @@ app.get('/headers', function (request, response) {
 app.get('/headers/:header_name', function (request, reponse) {
   var header_name = request.params.header_name;
   var header = request.headers;
-  var test = header.header_name;
+  var test = header[header_name];
   response.json({
     test
   });

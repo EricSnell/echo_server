3b12a9e4f55aaa629f17952f7daf5ead34209b73
Robby Kim <robert.st.kim@gmail.com>
2016-06-29 12:47:22 -0400

    figured out how to display the header

--- echo_server.js
+++ echo_server.js
@@ -3,23 +3,28 @@ var express = require('express');
 
 var app = express();
 
-app.get('/headers', function(request, response) {
-	request.headers;
-    response.json({request});
+app.get('/headers', function (request, response) {
+  var headers = request.headers;
+  response.json({
+    headers
+  });
 });
 
+
+app.get('/headers/:header_name', function (request, reponse) {
+  var header_name = request.params.header_name;
+  var header = request.headers;
+  var test = header.header_name;
+  response.json({
+    test
+  });
+});
 /*
-app.get('/headers/:header_name', function(request, reponse) {
-	response.json({
-
-	});
-});
-
 
 app.get('/version', function(request, response) {
-	response.json({
+  response.json({
 
-	});
+  });
 }); */
 
 app.listen(8080);
